a91ae31a1d571a07b6521e0108dc829afbb0f7dc
ali-ismail02 <alii.ismail02@gmail.com>
2022-12-04 00:00:18 +0200

    Frontend: Added ImageBackground style

--- frontend/styles/styles.js
+++ frontend/styles/styles.js
@@ -1,2 +1,13 @@
+import { Dimensions, StyleSheet } from "react-native";
+
+const { width, height } = Dimensions.get("window");
+
 export default StyleSheet.create({
+    ImageBackground: {
+        width: width,
+        height: height,
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
 });
