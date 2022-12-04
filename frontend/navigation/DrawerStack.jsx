c8401f663ff6f0c85c55fc8605428e93621dc7e3
ali-ismail02 <alii.ismail02@gmail.com>
2022-12-04 20:59:20 +0200

    Frontend: DrawerStack => Cleaned code, Added comments

--- frontend/navigation/DrawerStack.jsx
+++ frontend/navigation/DrawerStack.jsx
@@ -1,12 +1,10 @@
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from "@react-navigation/native";
 import React, { useRef } from "react";
-import { useSelector } from 'react-redux';
-import CustomDrawer from "./CustomDrawer";
 import Todos from '../screens/Todos';
+import CustomDrawer from "./CustomDrawer";
 
 export function DrawerStack() {
-  const user = useSelector((state) => state?.user);
   const Drawer = createDrawerNavigator();
   const navigationRef = useRef(null);
 
