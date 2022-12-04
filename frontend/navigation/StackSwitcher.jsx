3dfdc4e096a1a54aa1e2467083f6308e761186fe
ali-ismail02 <alii.ismail02@gmail.com>
2022-12-04 17:30:05 +0200

    Frontend: StackSwitcher => Called DrawerStack

--- frontend/navigation/StackSwitcher.jsx
+++ frontend/navigation/StackSwitcher.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { DrawerStack } from './DrawerStack';
 import {OnboardingStack} from './OnboardingStack';
 
 export function StackSwitcher() {
   const user = useSelector((state) => state?.user);
+  console.log(user);
 
   // for drawer navigation
-  return user?.userProfile ? null : <OnboardingStack />;
+  return user?.userProfile ? <DrawerStack /> : <OnboardingStack />;
 }
