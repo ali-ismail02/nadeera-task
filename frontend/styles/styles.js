84d3f57685d5a1e942ecb44335fa6914a49bac99
ali-ismail02 <alii.ismail02@gmail.com>
2022-12-04 16:09:32 +0200

    Frontend: styles => Added registerScreen styles

--- frontend/styles/styles.js
+++ frontend/styles/styles.js
@@ -17,4 +17,32 @@ export default StyleSheet.create({
         fontWeight: "bold",
         fontStyle: "italic",
     },
+    input: {
+        width:width*0.8,
+        height: 50,
+        backgroundColor: "#fff",
+        borderRadius: 5,
+        paddingHorizontal: 16,
+        fontSize: 16,
+        color: "#000",
+        marginVertical: 10,
+        borderColor: "#000",
+        borderWidth: 1,
+    },
+    registerImage: {
+        width: 100,
+        height: 100,
+        borderRadius: 100,
+        marginBottom: 20,
+    },
+    registerView: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    error: {
+        color: "#f00",
+        fontSize: 16,
+        fontWeight: "bold",
+    },
 });
